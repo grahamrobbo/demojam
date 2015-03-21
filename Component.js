21dc531482659bf671b2c02138ed2c83b63a77f4
Graham Robinson <graham@yelcho.com.au>
2015-03-21 17:08:24 +1100

    Initial commit

---
+++ Component.js
@@ -0,0 +1,49 @@
+sap.ui.core.UIComponent.extend('my.simple.app.Component', {
+    metadata: {
+        name: 'Sample my.simple.app app',
+        version: '1.0.0',
+        includes: ['css/styles.css'],
+        dependencies: {
+            libs: ['sap.m']
+        },
+        rootView: 'my.simple.app.view.App'
+    },
+
+    init: function() {
+
+        var that = this;
+        var oPreprocessors = {};
+
+        // when auto prefixing is enabled we add the prefix
+        if (this.getAutoPrefixId()) {
+            oPreprocessors.id = function(sId) {
+                return that.createId(sId);
+            };
+        }
+
+        // create the routing
+        var oMetadata = this.getMetadata();
+        // extend the metadata config, so that the metadata object cannot be modified afterwards
+        var oRoutingConfig = jQuery.extend({}, oMetadata.getRoutingConfig());
+        var aRoutes = oMetadata.getRoutes();
+
+        // create the router for the component instance
+        if (aRoutes) {
+            jQuery.sap.require("sap.ui.core.routing.Router");
+            var fnRouterConstructor = oRoutingConfig.routerClass || sap.ui.core.routing.Router;
+            if (typeof fnRouterConstructor === "string") {
+                fnRouterConstructor = jQuery.sap.getObject(fnRouterConstructor);
+            }
+
+            this._oRouter = new fnRouterConstructor(aRoutes, oRoutingConfig, this);
+        }
+
+        // create the content
+        this.runAsOwner(function() {
+            sap.ui.base.ManagedObject.runWithPreprocessors(function() {
+                that.setAggregation("rootControl", that.createContent());
+            }, oPreprocessors);
+        });
+
+    }
+});
---
+++ view/App.controller.js
@@ -0,0 +1,31 @@
+sap.ui.controller('my.simple.app.view.App', {
+
+    onInit: function() {
+        this.getView().setModel(new sap.ui.model.json.JSONModel({
+            cities: [{
+                id: "SYD",
+                name: "Sydney"
+            }, {
+                id: "MEL",
+                name: "Melbourne"
+            }],
+            items: [{
+                description: "Master data quality check",
+                errors: 4,
+                cityId: "A1"
+            }, {
+                description: "Pre payroll process",
+                errors: 8,
+                cityId: "A1"
+            }, {
+                description: "Errors from payroll run",
+                errors: 0,
+                cityId: "A2"
+            }, {
+                description: "Payroll data validations",
+                errors: 20,
+                cityId: "A1"
+            }]
+        }));
+    }
+});
